07fee83a59694af316a3e975822614eafa8f8bed
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-09-11 00:03:13 +0300

    Reviews

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Movies } from "./pages/Movies";
 import { Lauout } from "./Layout/Lauout";
+import { MovieDetails } from "./pages/MovieDetails";
 
 export const App = () => {
   return (
@@ -14,6 +15,11 @@ export const App = () => {
         <Route path="/" element={<Lauout/>}>
             <Route index element={<Home />} />
             <Route path="movies" element={<Movies />} />
+
+            <Route path='/movies/:movieId' element={<MovieDetails />} />
+
+
+            <Route path="*" element={<Home />} />
         </Route>
       </Routes>   
     </div>
---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cast = () => {
+  return (
+    <div>Cast</div>
+  )
+}
