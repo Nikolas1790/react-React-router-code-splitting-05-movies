24bc33b3ea8dd6b5184753275dedb0db3d64897a
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-02-18 15:16:15 +0200

    s

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies' > 
+    <BrowserRouter basename='/react-React-router-code-splitting-05-movies' > 
       <App />
     </BrowserRouter>
   </React.StrictMode>
